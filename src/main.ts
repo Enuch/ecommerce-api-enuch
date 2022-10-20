00dd2f39277a3a57f946de6c1fec67c64d662bbb
Enuch <enuchsantys@gmail.com>
2022-10-20 01:02:34 -0300

    Configurações de CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    maxAge: 3600,
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
